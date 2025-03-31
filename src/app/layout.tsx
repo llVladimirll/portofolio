709e9bc240a117785cc68892b5d16549d9869eb1
Vladimir <22523298@students.uii.ac.id>
2025-03-31 12:58:27 +0700

    p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,29 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import Navbar from "@/components/Navbar"
-import DarkModeToggle from "@/components/DarkModeToggle"
-import type React from "react" // Import React
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import type React from "react"; // Import React
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Your Name - Fullstack Developer",
-    description: "Portfolio website showcasing my skills and projects as a fullstack developer",
-}
+  title: "Vladimirrahman - Fullstack Developer",
+  description:
+    "Portfolio website showcasing my skills and projects as a fullstack developer",
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-        <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
+  return (
+    <html lang="en">
+      <body className={`${inter.className} dark:bg-gray-900 dark:text-white`}>
         <Navbar />
-        <div className="fixed top-4 right-4 z-50">
-            <DarkModeToggle />
-        </div>
+        <div className="fixed top-4 right-4 z-50"></div>
         {children}
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
-
